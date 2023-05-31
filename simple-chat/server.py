2a5701e079f85ba7346f208321ff2a37907c5718
kamelhanna <74700713+kamelhanna@users.noreply.github.com>
2023-05-31 09:55:21 +0300

    Update server.py

--- simple-chat/server.py
+++ simple-chat/server.py
@@ -1,1 +1,27 @@
+from socket import *
 
+s = socket(AF_INET, SOCK_STREAM)
+
+host = "127.0.0.1"
+port = 7002
+
+s.bind((host, port))
+
+s.listen(5)
+
+client, address = s.accept()
+
+print("connection from", address[0])
+
+while True: 
+    received_data = client.recv(2048)
+    if received_data.decode() == 'Q':
+        break
+    print("Client : ", received_data.decode('utf=8'))
+
+    text_input = input("server : ")
+    client.send(text_input.encode('utf=8'))
+    if text_input == 'Q':
+        break
+
+s.close()
